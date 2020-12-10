7abd91aac60a09e634810989eed5c52bca3afa9d
Arjun Gupta <arjun.g1511@gmail.com>
2020-12-10 04:31:23 -0500

    Testing

--- my_models/cnn_poison_pytorch_r2.py
+++ my_models/cnn_poison_pytorch_r2.py
@@ -48,6 +48,7 @@ def make_gtsrb_model(**kwargs) -> Net:
     return Net()
 
 def cross_entropy(outputs_x, targets_x) :
+    print('-----')
     return -torch.mean(torch.sum(outputs_x * targets_x, dim=1))
 
 def get_art_model(
